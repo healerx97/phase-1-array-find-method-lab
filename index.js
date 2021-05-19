758b20a2c68f892e1b48fb4aa05eadfc561cc856
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-19 01:54:18 -0400

    commit tree c5f3fa35c68a77ebe32f07e9f920a4a85b4dd648

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function win(array) {
+    if (array.result == "W") {
+        return true
+    }
+}
 
+function superbowlWin(array) {
+    if (array.find(win)) {
+        return array.find(win).year
+    }
+}
